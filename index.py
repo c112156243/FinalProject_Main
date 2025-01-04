7d2aeec1756ef07215677b3303ee2f83ee1fc305
chenhao175 <c112156214@nkust.edu.tw>
2025-01-05 02:32:55 +0800

    666

--- index.py
+++ index.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 
-
+hahahaha = "hahahaha"
 
 @app.route("/callback", methods=['POST'])
 def callback():
